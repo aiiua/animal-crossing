f80c40c8d99551cca82a6602c982450e773601c1
@Aiiua <347565645@qq.com>
2020-03-25 23:58:10 +0800

    feat: add image data

---
+++ src/data/images.js
@@ -0,0 +1,13 @@
+const images = {
+  '树枝': 'http://oss.aiiua.com/formula/pic_010.png',
+  '硬木材': 'http://oss.aiiua.com/formula/pic_013.png',
+  '软木材': 'http://oss.aiiua.com/formula/pic_015.png',
+  '木材': 'http://oss.aiiua.com/formula/pic_002.png',
+  '石头': 'http://oss.aiiua.com/formula/pic_009.png',
+  '铁矿石': 'http://oss.aiiua.com/formula/pic_003.png',
+  '金矿石': 'http://oss.aiiua.com/formula/pic_017.png',
+  '黏土': 'http://oss.aiiua.com/formula/pic_039.png',
+  '简陋捕虫网': 'http://oss.aiiua.com/formula/pic_011.png'
+}
+
+export default images
